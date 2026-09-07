d65f14cd8001eed85f738035a0ecd882d172e692
Mihail <mihailberdyugin@yandex.ru>
2026-09-07 08:49:04 +0300

    Update app-new.js

--- js/app-new.js
+++ js/app-new.js
@@ -1,5 +1,5 @@
 import { loadData, saveData } from './storage.js';
-import { renderAll, renderTodayScreen, renderTransactionList } from './ui-new.js';
+import { renderAll, renderTodayScreen, renderTransactionList, renderSettings } from './ui-new.js';
 import { formatDateKey } from './utils.js';
 
 // Список категорий расходов
