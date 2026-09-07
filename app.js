8eaffe9dde8f9a18f7af93a458b8c2f19e003928
Mihail <mihailberdyugin@yandex.ru>
2026-09-07 06:58:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -345,8 +345,6 @@ function handleAddPayment() {
     appData.fixedExpenses,
     appData.transactions,
   );
-  // Перезагружаем страницу для мобильных устройств (обход кэша)
-  location.reload();
 }
 
 // Обработчик клика по кнопкам удаления
